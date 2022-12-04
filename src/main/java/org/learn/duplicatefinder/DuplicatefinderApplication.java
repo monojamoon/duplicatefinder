1b7e97006de0a96770f77436a54116f656d7c2d1
Moon Jamoon <mono.jamoon@gmail.com>
2022-12-04 10:22:15 +0530

    added sqlite to project

---
+++ src/main/java/org/learn/duplicatefinder/DuplicatefinderApplication.java
@@ -0,0 +1,13 @@
+package org.learn.duplicatefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuplicatefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuplicatefinderApplication.class, args);
+	}
+
+}
